02f2b5513d856e0ade6d06f0a1ca6b229169379d
Dennis Patzer <d.patzer@heimrich-hannot.de>
2017-10-16 14:14:13 +0200

    fixed namepsaces

--- src/DependencyInjection/OverscrollExtension.php
+++ src/DependencyInjection/OverscrollExtension.php
@@ -2,7 +2,7 @@
 
 namespace HeimrichHannot\OverscrollBundle\DependencyInjection;
 
-use Behat\Gherkin\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
