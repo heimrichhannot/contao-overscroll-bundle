ebc3855f11dd38069da50b77e148893368dd56de
Dennis Patzer <d.patzer@heimrich-hannot.de>
2017-10-16 11:26:14 +0200

    fixed naming

---
+++ src/DependencyInjection/OverscrollExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HeimrichHannot\OverscrollBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class OverscrollExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container)
+    {
+    }
+}
